00d8ef72ab3ab9f496e1d593de83eab86bed7df9
maniacbug <maniacbug@ymail.com>
2011-08-28 11:56:06 -0700

    Add comments

--- internal/Commands.h
+++ internal/Commands.h
@@ -5,18 +5,44 @@
 #include <string>
 #include <map>
 
+/**
+ * Dispatches commands to workers
+ */
 class Commands
 {
 public:
   typedef bool (*worker)(const std::vector<std::string>&);
 protected:
-
+  /**
+   * Map of commands to workers
+   */
   std::map<std::string,worker> commandmap;
 
 public:
-
-  bool execute(const std::vector<std::string>&);
-  bool add(const std::string&,worker);
+  /**
+   * Executes the worker associated with the first token in @p commands 
+   *
+   * @param commands Tokenized command and parameters
+   *
+   * @retval true Worker function found and returned true
+   * @retval false Worker function not found or returned false
+   */
+  bool execute(const std::vector<std::string>& commands);
+  
+  /**
+   * Adds a worker
+   *
+   * @param command Single token for the command
+   * @param fn Worked function to execute for the command
+   *
+   * @retval true Worker added successfully
+   * @retval false Problema adding worker, not added
+   */
+  bool add(const std::string& command,worker fn);
+  
+  /**
+   * Empties the entire map
+   */
   void clear(void);
 };
 
