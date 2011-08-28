7f3022f9cb7767ac074104d22fad8722faa52b64
maniacbug <maniacbug@ymail.com>
2011-08-28 12:22:04 -0700

    Added testSet, can now set the pins from mock hardware

--- internal/Pins.h
+++ internal/Pins.h
@@ -1,10 +1,17 @@
 #ifndef __PINS_H__
 #define __PINS_H__
 
+#include <vector>
+
 class Pins
 {
+protected:
+  static const int num_pins = 20;
+  std::vector<int> digital_pin_states;
 public:
-  int digitalRead(int) { return 0; }
+  Pins(void) { digital_pin_states.reserve(num_pins); }
+  int digitalRead(int pin) { return digital_pin_states[pin]; }
+  void hwSet(int pin,int level) { digital_pin_states[pin] = level; }
 };
 
 #endif // __PINS_H__
