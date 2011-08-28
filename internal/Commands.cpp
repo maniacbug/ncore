9451d198698bef5d41a9a7460ec76fa7d71b4b2b
maniacbug <maniacbug@ymail.com>
2011-08-28 11:43:52 -0700

    testAddDuplicateWorker and testWorkerDispatchOk

--- internal/Commands.cpp
+++ internal/Commands.cpp
@@ -1,15 +1,34 @@
 #include <Commands.h>
 #include <vector>
 #include <string>
+#include <iostream>
 
-bool Commands::execute(const std::vector<std::string>&)
+using namespace std;
+
+bool Commands::execute(const std::vector<std::string>& _commands)
 {
-  return false;
+  bool result = false;
+  const string& command = ( _commands.size() > 0 ) ? _commands.at(0) : string();
+
+  if ( commandmap.count( command ) )
+  {
+    result = commandmap.at(command)(_commands);
+  }
+
+  return result;
 }
 
-bool Commands::add(const std::string&,worker)
+bool Commands::add(const std::string& _command, worker _fn)
 {
-  return true;
+  bool result = false;
+
+  if ( ! commandmap.count( _command ) )
+  {
+    commandmap[_command] = _fn;
+    result = true;
+  }
+
+  return result;
 }
 
 // vim:cin:ai:sts=2 sw=2 ft=cpp
--- internal/Commands.h
+++ internal/Commands.h
@@ -3,11 +3,17 @@
 
 #include <vector>
 #include <string>
+#include <map>
 
 class Commands
 {
 public:
   typedef bool (*worker)(const std::vector<std::string>&);
+protected:
+
+  std::map<std::string,worker> commandmap;
+
+public:
 
   bool execute(const std::vector<std::string>&);
   bool add(const std::string&,worker);
