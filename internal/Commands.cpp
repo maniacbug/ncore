f5375ba14c043a166c2f4b7d0fba69ec75051cde
maniacbug <maniacbug@ymail.com>
2011-08-28 11:10:19 -0700

    Started command processor

---
+++ internal/Commands.cpp
@@ -0,0 +1,10 @@
+#include <Commands.h>
+#include <vector>
+#include <string>
+
+bool Commands::execute(const std::vector<std::string>&)
+{
+  return false;
+}
+
+// vim:cin:ai:sts=2 sw=2 ft=cpp
---
+++ internal/Commands.h
@@ -0,0 +1,15 @@
+#ifndef __COMMANDS_H__
+#define __COMMANDS_H__
+
+#include <vector>
+#include <string>
+
+class Commands
+{
+public:
+  bool execute(const std::vector<std::string>&);
+};
+
+#endif // __COMMANDS_H__
+
+// vim:cin:ai:sts=2 sw=2 ft=cpp
