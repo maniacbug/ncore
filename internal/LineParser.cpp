9f06dd83f1c82d73f9734f89ffa0c0914c4a9c64
maniacbug <maniacbug@ymail.com>
2011-08-28 10:45:17 -0700

    Parses one token

--- internal/LineParser.cpp
+++ internal/LineParser.cpp
@@ -3,6 +3,9 @@
 using std::string;
 using std::vector;
 
-void LineParser::parse(const string&)
+void LineParser::parse(const string& _in)
 {
+  if ( _in.size() )
+    push_back(_in);
 }
+// vim:cin:ai:sts=2 sw=2 ft=cpp
