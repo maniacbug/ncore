13acb13bc4c300579ad78fcad306ba0ef0ca8702
maniacbug <maniacbug@ymail.com>
2011-09-02 00:00:00 -0700

    Created new serial buffer.  This will be the internal operation for the Arduino 'Serial' object

---
+++ internal/SerialBuffer.h
@@ -0,0 +1,14 @@
+#ifndef __SERIAL_BUFFER_H__
+#define __SERIAL_BUFFER_H__
+
+class Logger;
+
+class SerialBuffer
+{
+public:
+  SerialBuffer(Logger&) {}
+};
+
+#endif // __SERIAL_BUFFER_H__
+// vim:cin:ai:sts=2 sw=2 ft=cpp
+
