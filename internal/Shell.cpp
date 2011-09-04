baee9edb62937b9a81c0f73578459a14088db7ae
maniacbug <maniacbug@ymail.com>
2011-09-04 00:00:00 -0700

    Made testConcurrency to be reliable again, using a mutex

--- internal/Shell.cpp
+++ internal/Shell.cpp
@@ -27,7 +27,6 @@ void Shell::run(const Dispatcher& _commands, const Clock& _clock)
     char* input = readline(prompt.str().c_str());
     if (input)
     {
-      add_history(input);
       command = string(input);
     }
     else
@@ -35,15 +34,21 @@ void Shell::run(const Dispatcher& _commands, const Clock& _clock)
    
     if ( command != "quit" && command != "")
     {
+      bool valid = false;
       try
       {
 	if (!_commands.execute_new(Parser(command)))
 	  cout << "Command failed: " << command << endl;
+	else
+	  valid = true;
       }
       catch (runtime_error* e)
       {
 	cout << "Error: " << e->what() << endl;
       }
+      // Only add once we know it's a working command
+      if (valid)
+	add_history(input);
     }
   }
   while (command != "quit" );
