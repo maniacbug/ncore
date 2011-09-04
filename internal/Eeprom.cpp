145251e3451e1496226c016322a631793ffd101a
maniacbug <maniacbug@ymail.com>
2011-09-04 00:00:00 -0700

    Eeprom now logs

--- internal/Eeprom.cpp
+++ internal/Eeprom.cpp
@@ -1,9 +1,10 @@
 #include <Eeprom.h>
+#include <Logger.h>
 
 // eeprom size must always be a power of 2
 static const int eeprom_size = 1 << 9;
 
-Eeprom::Eeprom(void)
+Eeprom::Eeprom(Logger& _log): log(_log)
 {
   values.resize(eeprom_size);
 }
@@ -14,11 +15,11 @@ size_t Eeprom::size(void)
 uint8_t Eeprom::readByte(int addr) const
 {
   uint8_t result = values.at(addr);
-  //theLogger.sketch("EEPR","read %i = %u",addr,result);
+  log.sketch("EEPR","read %i = %u",addr,result);
   return result;
 }
 void Eeprom::writeByte(int addr, uint8_t value)
 {
-  //theLogger.sketch("EEPR","write %i = %u",addr,__value);
+  log.sketch("EEPR","write %i = %u",addr,value);
   values.at(addr) = value;
 }
--- internal/Eeprom.h
+++ internal/Eeprom.h
@@ -4,12 +4,15 @@
 #include <vector>
 #include <inttypes.h>
 
+class Logger;
+
 class Eeprom
 {
 private:
   std::vector<uint8_t> values;
+  Logger& log;
 public:
-  Eeprom(void);
+  Eeprom(Logger&);
   size_t size(void);
   uint8_t readByte(int addr) const;
   void writeByte(int addr, uint8_t value);
