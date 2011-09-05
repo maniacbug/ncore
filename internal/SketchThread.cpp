99c8f5a7342d0771b28942a182e987f452803a32
maniacbug <maniacbug@ymail.com>
2011-09-05 00:00:00 -0700

    Added a second 'custom' mode of operation for sketch thread

--- internal/SketchThread.cpp
+++ internal/SketchThread.cpp
@@ -9,15 +9,44 @@ extern "C" void loop(void);
 
 SketchThread::SketchThread(int mode): pthread(NULL)
 {
-
   // Regular operation, controlling setup() and loop()
   if ( mode == 0)
   {
     pthread = new pthread_t;
     pthread_create( pthread, NULL, sketch_thread_main, NULL );
   }
+  // Custom thread body, use SketchThread::startCustom to launch.
+  else if ( mode == 1 )
+  {
+  }
   else
     throw new std::runtime_error("Unknown thread mode");
+}
+
+void SketchThread::startCustom(void (*fn)(void))
+{
+  if ( pthread )
+    throw new std::runtime_error("Thread is already running");
+
+  if ( !fn )
+    throw new std::runtime_error("Must send a valid function");
+
+  custom_body = fn;
+  pthread = new pthread_t;
+  pthread_create( pthread, NULL, sketch_thread_custom_main, this );
+}
+
+void* SketchThread::sketch_thread_custom_main(void* pv)
+{
+  SketchThread* psk = reinterpret_cast<SketchThread*>(pv);
+
+  pthread_setcancelstate(PTHREAD_CANCEL_ENABLE,NULL);
+  pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS,NULL);
+
+  if ( psk && psk->custom_body )
+    psk->custom_body();
+
+  return NULL;
 }
 
 void* SketchThread::sketch_thread_main(void*)
--- internal/SketchThread.h
+++ internal/SketchThread.h
@@ -6,11 +6,14 @@ class SketchThread
 {
 private:
   pthread_t* pthread;
+  void (*custom_body)(void);
 protected:
   static void* sketch_thread_main(void*);
+  static void* sketch_thread_custom_main(void*);
 
 public:
   SketchThread(int mode=0);
+  void startCustom(void (*fn)(void));
   void waitToFinish(void);
   virtual ~SketchThread();
 };
