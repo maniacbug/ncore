7ee9c2dd5dccc659703fc1e1a410c06138986baf
maniacbug <maniacbug@ymail.com>
2011-09-05 00:00:00 -0700

    Added logging to spiqueue

--- internal/SpiQueue.cpp
+++ internal/SpiQueue.cpp
@@ -12,9 +12,12 @@ void SpiQueue::hwEnqueue(uint8_t _byte)
   qts.push(_byte);
 }
   
-uint8_t SpiQueue::transfer(uint8_t)
+uint8_t SpiQueue::transfer(uint8_t _in)
 {
+  if ( ! qts.available() )
+    logger.sketch("SPI","in %02x Waiting for output values",_in);
   uint8_t out = qts.pop();
+  logger.sketch("SPI","in %02x out %02x",_in,out);
   return out;
 }
 
