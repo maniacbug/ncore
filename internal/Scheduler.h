96a2b9258f89f076f513508d7c9d945a1051b02d
maniacbug <maniacbug@ymail.com>
2011-09-07 00:00:00 -0700

    Added new scheduler class (stub)

---
+++ internal/Scheduler.h
@@ -0,0 +1,14 @@
+#ifndef __SCHEDULER_H__
+#define __SCHEDULER_H__
+
+// STL includes
+// C includes
+// Library includes
+// Project includes
+
+class Scheduler
+{
+};
+
+#endif // __SCHEDULER_H__
+// vim:cin:ai:sts=2 sw=2 ft=cpp
