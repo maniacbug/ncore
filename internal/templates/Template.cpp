e9147bbd8bee656a735413d9a90d5ffab9cbf715
maniacbug <maniacbug@ymail.com>
2011-09-07 00:00:00 -0700

    Templates for new files

---
+++ internal/templates/Template.cpp
@@ -0,0 +1,7 @@
+// STL includes
+// C includes
+// Library includes
+// Project includes
+#include <Template.h>
+
+// vim:cin:ai:sts=2 sw=2 ft=cpp
---
+++ internal/templates/Template.h
@@ -0,0 +1,14 @@
+#ifndef __TEMPLATE_H__
+#define __TEMPLATE_H__
+
+// STL includes
+// C includes
+// Library includes
+// Project includes
+
+class Template
+{
+};
+
+#endif // __TEMPLATE_H__
+// vim:cin:ai:sts=2 sw=2 ft=cpp
