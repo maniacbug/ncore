ce8001f8e924da400836f642ffc085aff4e37f43
maniacbug <maniacbug@ymail.com>
2011-09-13 00:00:00 -0700

    Separated out mock_time into its own module

---
+++ internal/test/mock_time.cpp
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <sys/time.h>
+#include <string.h>
+
+struct timeval mock_time = {0,0};
+
+extern "C" {
+
+  void mock_time_reset(void)
+  {
+    memset(&mock_time,0,sizeof(mock_time));
+  }
+
+  int usleep(useconds_t us)
+  {
+    mock_time.tv_usec += us;
+    while ( mock_time.tv_usec >= 1000000L )
+    {
+      mock_time.tv_sec ++;
+      mock_time.tv_usec -= 1000000L;
+    }
+    return 0;
+  }
+
+  int gettimeofday(struct timeval *tv, struct timezone *)
+  {
+    memcpy(tv,&mock_time,sizeof(mock_time));
+    return 0;
+  }
+
+}
+
