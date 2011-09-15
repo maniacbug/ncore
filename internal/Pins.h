5dd315eaf3437444e35915990c294baeff9fcf6e
maniacbug <maniacbug@ymail.com>
2011-09-15 08:21:35 -0700

    Copyright headers

--- internal/Pins.h
+++ internal/Pins.h
@@ -1,3 +1,12 @@
+
+/*
+ Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+
 #ifndef __PINS_H__
 #define __PINS_H__
 
