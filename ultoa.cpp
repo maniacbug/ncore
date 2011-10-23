051e7456487293acf5d953e0fcb257ba39757065
maniacbug <maniacbug@ymail.com>
2011-10-23 15:14:44 -0700

    Remove warnings

--- ultoa.cpp
+++ ultoa.cpp
@@ -35,9 +35,32 @@ template <class T>
 		return buf;
 	}
 
+template <class T>
+	std::string __utoa(T value, int base) {
+	
+		std::string buf;
+	
+		// check that the base if valid
+		if (base < 2 || base > 16) return buf;
+
+		enum { kMaxDigits = 35 };
+		buf.reserve( kMaxDigits ); // Pre-allocate enough space.
+	
+		int quotient = value;
+	
+		// Translating number to string with base:
+		do {
+			buf += "0123456789abcdef"[ std::abs( quotient % base ) ];
+			quotient /= base;
+		} while ( quotient );
+	
+		std::reverse( buf.begin(), buf.end() );
+		return buf;
+	}
+
 char* ultoa( unsigned long __val, char* __s, int __radix )
 {
-	std::string result = __toa(__val,__radix);
+	std::string result = __utoa(__val,__radix);
 	strcpy(__s,result.c_str());
 
 	return __s;
@@ -58,7 +81,7 @@ char* itoa( int __val, char* __s, int __radix )
 }
 char* utoa( unsigned int __val, char* __s, int __radix )
 {
-	std::string result = __toa(__val,__radix);
+	std::string result = __utoa(__val,__radix);
 	strcpy(__s,result.c_str());
 
 	return __s;
