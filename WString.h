7d135fa65d955e35b0051cb6d82474e52f700564
maniacbug <maniacbug@ymail.com>
2011-10-24 20:23:09 -0700

    Use ultoa.h

--- WString.h
+++ WString.h
@@ -27,6 +27,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <avr/pgmspace.h>
+#include "ultoa.h"
 
 // When compiling programs with this class, the following gcc parameters
 // dramatically increase performance and memory (RAM) efficiency, typically
