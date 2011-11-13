caf682b97a95044a1e17472eb50a8a882b728ca0
maniacbug <maniacbug@ymail.com>
2011-11-13 12:03:52 -0800

    Added copyright header to templates, and test template creates a ptr to tempalte object

--- internal/templates/Template.h
+++ internal/templates/Template.h
@@ -1,3 +1,11 @@
+/*
+ Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+
 #ifndef __TEMPLATE_H__
 #define __TEMPLATE_H__
 
