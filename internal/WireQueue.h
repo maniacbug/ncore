419f40417d54b109b23963c67e1bde64b31311fb
maniacbug <maniacbug@ymail.com>
2011-11-20 18:41:32 -0800

    Stubs for WireQueue

---
+++ internal/WireQueue.h
@@ -0,0 +1,30 @@
+/*
+ Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+
+#ifndef __WIREQUEUEi_H__
+#define __WIREQUEUEi_H__
+
+// STL headers
+// C headers
+// Framework headers
+// Library headers
+// Project headers
+
+/**
+ * Example for how classes should be declared
+ */
+
+class WireQueue
+{
+private:
+protected:
+public:
+};
+
+#endif // __WIREQUEUEi_H__
+// vim:cin:ai:sts=2 sw=2 ft=cpp
