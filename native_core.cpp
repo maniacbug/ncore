09fc1685a0bf9d8d43845e1c87aaa6e2bd275e8f
Grégory Salvan <apieum@gmail.com>
2015-05-31 15:45:23 +0200

    Remove obsolete or moved functions in native core

--- native_core.cpp
+++ native_core.cpp
@@ -18,9 +18,10 @@ extern Logger theLogger;
 extern Pins thePins;
 extern Clock theClock;
 
+
 extern "C" {
 
-void __cxa_pure_virtual()
+void __cxa_pure_virtual(void)
 {
   theLogger.sketch("CORE","SYSTEM HALT -- cxa_pure_virtual");
   while (1) {}
@@ -71,11 +72,6 @@ void pinMode(uint8_t pin,uint8_t mode)
   thePins.pinMode(pin,mode);
 }
 
-void pinSymbol(uint8_t pin,const char* label)
-{
-  thePins.pinSymbol(pin,label);
-}
-
 void attachInterrupt(uint8_t num, void (*fn)(void), int)
 {
   thePins.attachInterrupt(num,fn);
@@ -124,32 +120,16 @@ void printf_P(const char* format,...)
 
 unsigned long pulseIn(uint8_t pin, uint8_t /*state*/, unsigned long /*timeout*/ )
 {
-  theLogger.sketch("CORE","%i: pulseIn 1000",pin);
+  theLogger.sketch("CORE", "%i: pulseIn 1000", pin);
 
   return 1000LU;
 }
 
-void analogWrite(uint8_t pin,int level)
+void analogWrite(uint8_t pin, int level)
 {
-  theLogger.sketch("PINS","%i: %i",pin,level);
-}
-
-void fdevopen(int (*)(char, FILE*),int)
-{
+  theLogger.sketch("PINS", "%i: %i",pin,level);
 }
 
 }
-
-void tone(uint8_t pin, unsigned int what, unsigned long wait)
-{
-  theLogger.sketch("PINS","%i: tone %u",pin,what);
-  delay(wait);
-}
-
-void noTone(uint8_t pin)
-{
-  theLogger.sketch("PINS","%i: noTone",pin);
-}
-
 // vim:cin:ai:sts=2 sw=2 ft=cpp
 
